abef68ee384d6ed5853d2dd0787f5567bfa0a70d
Lucky038 <rachkov.mv@phystech.edu>
2023-10-10 20:09:08 +0300

    2.0 runners

--- 6kz/runners.cpp
+++ 6kz/runners.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <cstddef>
 #include <sys/types.h>
 #include <sys/ipc.h>
@@ -21,27 +22,25 @@ const uint64_t TIME_PER_RUNNER = 10;
 
 const long MSG_SIZE = 0;
 
-void runner (uint64_t amnt_runner, id_t id);
+const long JUDGE_TYPE = LONG_MAX;
+
+void runner (uint64_t cnt_runner, id_t id);
 void judge (uint64_t amnt_runners, id_t id);
 
 int main ()
 {
-    const uint64_t amnt_runners = 10;
+    const uint64_t amnt_runners = 5;
 
     id_t id = msgget (IPC_PRIVATE, S_IRWXU | S_IRWXG | S_IRWXO);
 
-    if (fork () == 0)
+    for (uint64_t cnt = 1; cnt != amnt_runners + 1; ++cnt)
     {
-        for (uint64_t cnt = 1; cnt != amnt_runners + 1; ++cnt)
-        {
-            runner(cnt, id);
-        }
-        exit (0);
+        runner(cnt, id);
     }
 
     judge (amnt_runners, id);
 
-    for (uint64_t cnt = 0; cnt != amnt_runners; ++cnt)
+    for (uint64_t cnt = 1; cnt != amnt_runners + 1; ++cnt)
     {
         wait (NULL);
     }
@@ -53,41 +52,33 @@ void runner (uint64_t cnt_runner, id_t id)
 {
     if (fork() == 0)
     {
-        Message msg;
-
+        Message msg_to_snd;
+        msg_to_snd.type = JUDGE_TYPE;
         printf ("R%ld: I came!\n", cnt_runner);
-
-        msg.type = cnt_runner;
-        if (msgsnd(id, (const void *) &msg, MSG_SIZE, 0) < 0)
+        if (msgsnd(id, (const void *) &msg_to_snd, MSG_SIZE, 0) < 0)
         {
             perror ("name snd");
         }
 
-        Message buf;
-        if (msgrcv(id, &buf, MSG_SIZE, cnt_runner, 0) < 0)
+        Message msg_to_rcv;
+        if (msgrcv(id, &msg_to_rcv, MSG_SIZE, cnt_runner, 0) < 0)
         {
             perror ("start rcv");
         }
-
         printf ("R%ld: Started!\n", cnt_runner);
+
 
         usleep (TIME_PER_RUNNER);
 
+        msg_to_snd.type = cnt_runner + 1;
         printf ("R%ld: Finished!\n", cnt_runner);
-
-        msg.type = cnt_runner + 1;
-        if (msgsnd(id, (const void *) &msg, MSG_SIZE, 0) < 0)
+        if (msgsnd(id, (const void *) &msg_to_snd, MSG_SIZE, 0) < 0)
         {
             perror ("finish snd");
         }
 
         exit (0);
     }
-
-    // if (cnt_runner != amnt_runners - 1)
-    // {
-    //     runner (cnt_runner + 1, amnt_runners, id);
-    // }
 }
 
 void judge (uint64_t amnt_runners, id_t id)
@@ -102,8 +93,8 @@ void judge (uint64_t amnt_runners, id_t id)
         {
             perror ("");
         }
+        printf("J: Ok %ld/%ld\n", cnt, amnt_runners);
 
-        printf("J: Ok %ld/%ld\n", buf.type, amnt_runners);
     }
 
 
@@ -112,4 +103,9 @@ void judge (uint64_t amnt_runners, id_t id)
     buf.type = 1;
     msgsnd(id, &buf, MSG_SIZE, 0);
 
+    if (msgrcv(id, &buf, MSG_SIZE, amnt_runners + 1, 0) < 0)
+    {
+        perror ("");
+    }
+    printf ("J: FINISH\n");
 }
